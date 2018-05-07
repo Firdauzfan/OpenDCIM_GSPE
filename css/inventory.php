7cd5f1a660526bf9deeac62f6fef2396f7cebbee
root <ahmadjourdan@gmail.com>
2018-05-07 09:00:50 +0700

    ganti style css

--- css/inventory.php
+++ css/inventory.php
@@ -506,8 +506,8 @@ div.error {margin-top: 2em;margin-bottom: 2em;border: 1px dotted gray;}
 div.cabinet {
 	display: inline-block;
 	vertical-align: top;
-	min-width: 110px;
-	max-width: 120px;
+	min-width: 118px;
+	max-width: 128px;
 	margin-right: 80px;
 }
 
